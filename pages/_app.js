71f5c7c592e44b12f28f9d67f7170656c14d3ae1
nunawa <71294849+nunawa@users.noreply.github.com>
2023-02-10 15:57:56 +0900

    Add prototype page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { SSRProvider } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
 }
