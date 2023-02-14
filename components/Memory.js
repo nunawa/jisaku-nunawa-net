7dab100d08e9344913fcf61ff89697d69053380a
nunawa <71294849+nunawa@users.noreply.github.com>
2023-02-15 01:13:21 +0900

    Make it possible to sort and filter

--- components/Memory.js
+++ components/Memory.js
@@ -6,6 +6,7 @@ import InputGroup from "react-bootstrap/InputGroup";
 import Stack from "react-bootstrap/Stack";
 import Spinner from "react-bootstrap/Spinner";
 import useSWR from "swr";
+import { useState, useEffect, useRef } from "react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -22,70 +23,147 @@ function useProducts() {
   };
 }
 
-function Products() {
-  const { products, isLoading } = useProducts();
-  if (isLoading) {
+function Products({ products }) {
+  let productList = [];
+  console.log(products);
+
+  if (products) {
+    for (const iterator of products.slice(0, 30)) {
+      productList.push(
+        <Card className="mb-2">
+          <Card.Header as="h5">
+            <Stack direction="horizontal">
+              <div className="me-auto">
+                {iterator.manufacturer} {iterator.name}
+              </div>
+              <Button variant="primary">追加</Button>
+            </Stack>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>￥{iterator.price.toLocaleString()}</Card.Title>
+            <Card.Text style={{ wordBreak: "keep-all" }}>
+              売れ筋:&nbsp;{iterator.sales_rank}位&emsp;容量:&nbsp;
+              {iterator.capacity}&emsp;枚数:&nbsp;{iterator.pcs}
+              &emsp;規格:&nbsp;
+              {iterator.standard}
+              &emsp;インターフェース:&nbsp;{iterator.interface}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      );
+    }
+
+    return productList;
+  } else {
     return (
-      <Container className="px-auto py-auto">
+      <Container style={{ textAlign: "center" }}>
         <Spinner animation="border" />
       </Container>
     );
   }
-
-  console.log(products);
-
-  let productList = [];
-
-  for (const iterator of products) {
-    productList.push(
-      <Card className="mb-2">
-        <Card.Header as="h5">
-          <Stack direction="horizontal">
-            <div className="me-auto">
-              {iterator.manufacturer} {iterator.name}
-            </div>
-            <Button variant="primary">追加</Button>
-          </Stack>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>￥{iterator.price.toLocaleString()}</Card.Title>
-          <Card.Text style={{ wordBreak: "keep-all" }}>
-            売れ筋:&nbsp;{iterator.sales_rank}位&emsp;容量:&nbsp;
-            {iterator.capacity}&emsp;枚数:&nbsp;{iterator.pcs}
-            &emsp;規格:&nbsp;
-            {iterator.standard}
-            &emsp;インターフェース:&nbsp;{iterator.interface}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  }
-
-  return productList;
 }
 
 export default function Memory() {
+  const { products } = useProducts();
+  const [convertedProducts, setProducts] = useState();
+
+  useEffect(() => {
+    setProducts(products);
+  }, [products]);
+
+  const sort = useRef();
+
+  const keyword = useRef();
+
+  const max = useRef();
+  const min = useRef();
+
+  function handleSearch() {
+    let filteredProducts = products;
+
+    if (sort.current.value == "sales_rank_asc") {
+      filteredProducts = filteredProducts.slice().sort((a, b) => {
+        if (a.sales_rank === b.sales_rank) return 0;
+        if (a.sales_rank === null) return 1;
+        if (b.sales_rank === null) return -1;
+        return a.sales_rank < b.sales_rank ? -1 : 1;
+      });
+    } else if (sort.current.value == "price_asc") {
+      filteredProducts = filteredProducts
+        .slice()
+        .sort((a, b) => a.price - b.price);
+    }
+
+    if (keyword.current.value) {
+      filteredProducts = filteredProducts.filter((e) => {
+        const name = e.manufacturer + " " + e.name;
+        return name.indexOf(keyword.current.value) != -1;
+      });
+    }
+
+    const maxNum = Number(max.current.value);
+    const minNum = Number(min.current.value);
+    if (maxNum && minNum) {
+      filteredProducts = filteredProducts.filter(
+        (e) => e.price >= minNum && e.price <= maxNum
+      );
+    } else if (maxNum) {
+      filteredProducts = filteredProducts.filter((e) => e.price <= maxNum);
+    } else if (minNum) {
+      filteredProducts = filteredProducts.filter((e) => e.price >= minNum);
+    }
+
+    setProducts(filteredProducts);
+  }
+
   return (
     <>
       <InputGroup size="sm" className="mb-3">
-        <Form.Select style={{ maxWidth: "100px" }}>
-          <option value="1">売上順</option>
-          <option value="2">価格順</option>
+        <Form.Select
+          style={{ maxWidth: "100px" }}
+          ref={sort}
+          onChange={() => handleSearch()}
+        >
+          <option value="sales_rank_asc" selected>
+            売上順
+          </option>
+          <option value="price_asc">価格順</option>
         </Form.Select>
-        <Form.Control placeholder="キーワード" />
-        <Button variant="outline-secondary">クリア</Button>
-        <Button variant="outline-secondary">検索</Button>
+        <Form.Control ref={keyword} placeholder="キーワード" />
+        <Button
+          variant="outline-secondary"
+          onClick={() => {
+            keyword.current.value = "";
+            handleSearch();
+          }}
+        >
+          クリア
+        </Button>
+        <Button variant="outline-secondary" onClick={() => handleSearch()}>
+          検索
+        </Button>
       </InputGroup>
       <InputGroup size="sm" className="mb-3">
-        <InputGroup.Text id="basic-addon1">￥</InputGroup.Text>
-        <Form.Control placeholder="以上" />
-        <InputGroup.Text id="basic-addon1">～</InputGroup.Text>
-        <Form.Control placeholder="以下" />
-        <Button variant="outline-secondary">クリア</Button>
-        <Button variant="outline-secondary">検索</Button>
+        <InputGroup.Text>￥</InputGroup.Text>
+        <Form.Control ref={min} type="number" placeholder="以上" />
+        <InputGroup.Text>～</InputGroup.Text>
+        <Form.Control ref={max} type="number" placeholder="以下" />
+        <Button
+          variant="outline-secondary"
+          onClick={() => {
+            min.current.value = "";
+            max.current.value = "";
+            handleSearch();
+          }}
+        >
+          クリア
+        </Button>
+        <Button variant="outline-secondary" onClick={() => handleSearch()}>
+          検索
+        </Button>
       </InputGroup>
       <Container style={{ height: "70vh" }} className="overflow-auto">
-        <Products />
+        <Products products={convertedProducts} />
       </Container>
     </>
   );
--- pages/api/products.js
+++ pages/api/products.js
@@ -9,30 +9,31 @@ export default async function handler(req, res) {
   } else if (type == "cpu") {
     query = {
       query:
-        "SELECT TOP 30 c.name, c.price, c.sales_rank, c.manufacturer, c.frequency, c.socket, c.core_count, c.thread_count FROM PcParts p JOIN c IN p.cpu",
+        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.frequency, c.socket, c.core_count, c.thread_count FROM PcParts p JOIN c IN p.cpu",
     };
   } else if (type == "memory") {
     query = {
       query:
-        "SELECT TOP 30 c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.pcs, c.standard, c.interface FROM PcParts p JOIN c IN p.memory",
+        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.pcs, c.standard, c.interface FROM PcParts p JOIN c IN p.memory",
     };
   } else if (type == "motherboard") {
     query = {
       query:
-        "SELECT TOP 30 c.name, c.price, c.sales_rank, c.manufacturer, c.form_factor, c.socket, c.chipset, c.memory FROM PcParts p JOIN c IN p.motherboard",
+        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.form_factor, c.socket, c.chipset, c.memory FROM PcParts p JOIN c IN p.motherboard",
     };
   } else if (type == "gpu") {
     query = {
       query:
-        "SELECT TOP 30 c.name, c.price, c.sales_rank, c.manufacturer, c.gpu_name, c.bus_interface, c.memory, c.monitor FROM PcParts p JOIN c IN p.gpu",
+        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.gpu_name, c.bus_interface, c.memory, c.monitor FROM PcParts p JOIN c IN p.gpu",
     };
   } else if (type == "ssd") {
     query = {
       query:
-        "SELECT TOP 30 c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.cell_type, c.size, c.interface FROM PcParts p JOIN c IN p.ssd",
+        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.cell_type, c.size, c.interface FROM PcParts p JOIN c IN p.ssd",
     };
   } else {
     res.status(500).json({ error: "type param error" });
+    return;
   }
 
   const key = process.env.COSMOS_KEY;
