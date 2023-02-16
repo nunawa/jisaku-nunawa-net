ad2a8e3726809991047967d69f965c470339f89e
nunawa <71294849+nunawa@users.noreply.github.com>
2023-02-17 03:48:58 +0900

    Use @cfworker/cosmos instead of @azure/cosmos

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,4 +1,4 @@
-import { CosmosClient } from "@azure/cosmos";
+import { CosmosClient } from "@cfworker/cosmos";
 
 export default async function handler(req) {
   let query;
@@ -17,30 +17,15 @@ export default async function handler(req) {
       }
     );
   } else if (type == "cpu") {
-    query = {
-      query:
-        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.frequency, c.socket, c.core_count, c.thread_count FROM PcParts p JOIN c IN p.cpu",
-    };
+    query = `SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.frequency, c.socket, c.core_count, c.thread_count FROM ROOT p JOIN c IN p.cpu`;
   } else if (type == "memory") {
-    query = {
-      query:
-        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.pcs, c.standard, c.interface FROM PcParts p JOIN c IN p.memory",
-    };
+    query = `SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.pcs, c.standard, c.interface FROM PcParts p JOIN c IN p.memory`;
   } else if (type == "motherboard") {
-    query = {
-      query:
-        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.form_factor, c.socket, c.chipset, c.memory FROM PcParts p JOIN c IN p.motherboard",
-    };
+    query = `SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.form_factor, c.socket, c.chipset, c.memory FROM PcParts p JOIN c IN p.motherboard`;
   } else if (type == "gpu") {
-    query = {
-      query:
-        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.gpu_name, c.bus_interface, c.memory, c.monitor FROM PcParts p JOIN c IN p.gpu",
-    };
+    query = `SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.gpu_name, c.bus_interface, c.memory, c.monitor FROM PcParts p JOIN c IN p.gpu`;
   } else if (type == "ssd") {
-    query = {
-      query:
-        "SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.cell_type, c.size, c.interface FROM PcParts p JOIN c IN p.ssd",
-    };
+    query = `SELECT c.name, c.price, c.sales_rank, c.manufacturer, c.capacity, c.cell_type, c.size, c.interface FROM PcParts p JOIN c IN p.ssd`;
   } else {
     return new Response(
       JSON.stringify({
@@ -55,30 +40,18 @@ export default async function handler(req) {
     );
   }
 
-  const key = process.env.COSMOS_KEY;
+  const accountKey = process.env.COSMOS_KEY;
   const endpoint = process.env.COSMOS_ENDPOINT;
-  const client = new CosmosClient({ endpoint, key });
-  await client
-    .database("Main")
-    .container("PcParts")
-    .items.query(query)
-    .fetchAll()
-    .then(({ resources: results }) => {
-      return new Response(JSON.stringify(results), {
-        status: 200,
-        headers: {
-          "content-type": "application/json",
-          "cache-control": "public, s-maxage=1200, stale-while-revalidate=600",
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      return new Response(JSON.stringify({ error: "DB server error" }), {
-        status: 500,
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-    });
+  const dbId = "Main";
+  const collId = "PcParts";
+  const client = new CosmosClient({ endpoint, accountKey, dbId, collId });
+
+  const res = await client.queryDocuments({ query });
+  return new Response(res.body, {
+    status: res.status,
+    headers: {
+      "content-type": "application/json",
+      "cache-control": "public, s-maxage=1200, stale-while-revalidate=600",
+    },
+  });
 }
