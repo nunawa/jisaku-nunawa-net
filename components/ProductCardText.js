87de969785e3b41e5c37fc1ef375706369b259c8
nunawa <71294849+nunawa@users.noreply.github.com>
2023-12-31 22:36:23 +0900

    Improve spacing

--- components/ProductCardText.js
+++ components/ProductCardText.js
@@ -1,58 +1,98 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Badge, Card } from "react-bootstrap";
 
 export default function ProductCardText({ type, product }) {
   switch (type) {
     case "cpu":
       return (
-        <Card.Text style={{ wordBreak: "keep-all" }}>
-          <Badge>{product.sales_rank}位</Badge>&nbsp;
-          <Badge bg="secondary">{product.frequency}GHz</Badge>&nbsp;
-          <Badge bg="secondary">{product.socket}</Badge>&nbsp;
-          <Badge bg="secondary">{product.core_count}コア</Badge>&nbsp;
-          <Badge bg="secondary">{product.thread_count}スレッド</Badge>
+        <Card.Text>
+          <Badge className="me-1">{product.sales_rank}位</Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.frequency}GHz
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.socket}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.core_count}コア
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.thread_count}スレッド
+          </Badge>
         </Card.Text>
       );
 
     case "memory":
       return (
         <Card.Text style={{ wordBreak: "keep-all" }}>
-          <Badge>{product.sales_rank}位</Badge>&nbsp;
-          <Badge bg="secondary">{product.capacity}</Badge>&nbsp;
-          <Badge bg="secondary">{product.pcs}枚</Badge>&nbsp;
-          <Badge bg="secondary">{product.standard}</Badge>&nbsp;
-          <Badge bg="secondary">{product.interface}</Badge>
+          <Badge className="me-1">{product.sales_rank}位</Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.capacity}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.pcs}枚
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.standard}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.interface}
+          </Badge>
         </Card.Text>
       );
 
     case "motherboard":
       return (
         <Card.Text style={{ wordBreak: "keep-all" }}>
-          <Badge>{product.sales_rank}位</Badge>&nbsp;
-          <Badge bg="secondary">{product.form_factor}</Badge>&nbsp;
-          <Badge bg="secondary">{product.socket}</Badge>&nbsp;
-          <Badge bg="secondary">{product.chipset}</Badge>&nbsp;
-          <Badge bg="secondary">{product.memory}</Badge>
+          <Badge className="me-1">{product.sales_rank}位</Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.form_factor}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.socket}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.chipset}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.memory}
+          </Badge>
         </Card.Text>
       );
 
     case "gpu":
       return (
         <Card.Text style={{ wordBreak: "keep-all" }}>
-          <Badge>{product.sales_rank}位</Badge>&nbsp;
-          <Badge bg="secondary">{product.bus_interface}</Badge>&nbsp;
-          <Badge bg="secondary">{product.memory}</Badge>&nbsp;
-          <Badge bg="secondary">{product.monitor}</Badge>
+          <Badge className="me-1">{product.sales_rank}位</Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.bus_interface}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.memory}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.monitor}
+          </Badge>
         </Card.Text>
       );
 
     case "ssd":
       return (
         <Card.Text style={{ wordBreak: "keep-all" }}>
-          <Badge>{product.sales_rank}位</Badge>&nbsp;
-          <Badge bg="secondary">{product.capacity}</Badge>&nbsp;
-          <Badge bg="secondary">{product.cell_type}</Badge>&nbsp;
-          <Badge bg="secondary">{product.size}</Badge>&nbsp;
-          <Badge bg="secondary">{product.interface}</Badge>
+          <Badge className="me-1">{product.sales_rank}位</Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.capacity}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.cell_type}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.size}
+          </Badge>
+          <Badge className="me-1" bg="secondary">
+            {product.interface}
+          </Badge>
         </Card.Text>
       );
 
