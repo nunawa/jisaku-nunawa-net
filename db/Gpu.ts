2488627afd4bc04a16002fcc8c140fd949513fd9
nunawa <71294849+nunawa@users.noreply.github.com>
2025-02-11 02:20:55 +0900

    Add TypeORM Entities

---
+++ db/Gpu.ts
@@ -0,0 +1,49 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Gpu {
+  @PrimaryColumn("text")
+  id!: string;
+
+  @Column("text", { nullable: true })
+  name!: string | null;
+
+  @Column("bigint", { nullable: true })
+  price!: number | null;
+
+  @Column("bigint", { nullable: true })
+  sales_rank!: number | null;
+
+  @Column("float", { nullable: true })
+  review_score!: number | null;
+
+  @Column("bigint", { nullable: true })
+  bbs_count!: number | null;
+
+  @Column("text", { nullable: true })
+  date!: string | null;
+
+  @Column("text", { nullable: true })
+  manufacturer!: string | null;
+
+  @Column("text", { nullable: true })
+  gpu_name!: string | null;
+
+  @Column("text", { nullable: true })
+  bus_interface!: string | null;
+
+  @Column("text", { nullable: true })
+  slot!: string | null;
+
+  @Column("text", { nullable: true })
+  standard!: string | null;
+
+  @Column("text", { nullable: true })
+  capacity!: string | null;
+
+  @Column("text", { nullable: true })
+  monitor!: string | null;
+
+  @Column("bigint", { nullable: true })
+  score!: number | null;
+}
