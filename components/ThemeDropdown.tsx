771641b76e9d3b3a033effe555f52fd9a7d50fcb
nunawa <71294849+nunawa@users.noreply.github.com>
2025-03-23 00:42:53 +0900

    Use useMantineColorScheme hook

--- components/ThemeDropdown.tsx
+++ components/ThemeDropdown.tsx
@@ -1,68 +1,37 @@
-import { themeAtom } from "@/jotai/atom";
-import { useAtom } from "jotai";
-import { useEffect } from "react";
-import { NavDropdown } from "react-bootstrap";
-import { BsMoonFill, BsPcDisplay, BsSunFill } from "react-icons/bs";
+import {
+  ActionIcon,
+  Box,
+  Tooltip,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
 
 export function ThemeDropdown() {
-  const [theme, setThemeAtom] = useAtom(themeAtom);
-
-  useEffect(() => {
-    let targetTheme = theme;
-
-    if (theme === "default") {
-      targetTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
-    }
-
-    if (targetTheme === "light") {
-      document.documentElement.dataset.bsTheme = targetTheme;
-    } else if (targetTheme === "dark") {
-      document.documentElement.dataset.bsTheme = targetTheme;
-    }
-  }, [theme]);
-
-  function setTheme(t: string) {
-    if (t === "default") {
-      const defaultTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : "light";
-      document.documentElement.dataset.bsTheme = defaultTheme;
-      setThemeAtom("default");
-    } else if (t === "light") {
-      document.documentElement.dataset.bsTheme = "light";
-      setThemeAtom("light");
-    } else if (t === "dark") {
-      document.documentElement.dataset.bsTheme = "dark";
-      setThemeAtom("dark");
-    }
-  }
-
-  function ThemeIcon({ t }: { t: string }) {
-    if (t === "default") {
-      return <BsPcDisplay />;
-    } else if (t === "light") {
-      return <BsSunFill />;
-    } else if (t === "dark") {
-      return <BsMoonFill />;
-    }
-  }
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
 
   return (
-    <>
-      <NavDropdown title={<ThemeIcon t={theme} />}>
-        <NavDropdown.Item onClick={() => setTheme("light")}>
-          Light
-        </NavDropdown.Item>
-        <NavDropdown.Item onClick={() => setTheme("dark")}>
-          Dark
-        </NavDropdown.Item>
-        <NavDropdown.Item onClick={() => setTheme("default")}>
-          System
-        </NavDropdown.Item>
-      </NavDropdown>
-    </>
+    <Tooltip
+      label={computedColorScheme === "light" ? "ライトモード" : "ダークモード"}
+    >
+      <ActionIcon
+        onClick={() =>
+          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        }
+        variant="default"
+        size="lg"
+        aria-label="Toggle color scheme"
+      >
+        <Box darkHidden>
+          <BsSunFill />
+        </Box>
+        <Box lightHidden>
+          <BsMoonFill />
+        </Box>
+      </ActionIcon>
+    </Tooltip>
   );
 }
