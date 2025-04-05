99a31a0f9925fb967c9a213a967f7f0d3dac7cfe
nunawa <71294849+nunawa@users.noreply.github.com>
2025-04-06 01:12:42 +0900

    Remove @swc-jotai/react-refresh which causes error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    swcPlugins: [["@swc-jotai/react-refresh", {}]],
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
