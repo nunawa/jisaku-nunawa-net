f60a2a017f8c83499b79fcd2c8858f7d34012876
nunawa <71294849+nunawa@users.noreply.github.com>
2025-04-24 23:59:13 +0900

    Add restoration from query params to build page

--- components/BuildMain.tsx
+++ components/BuildMain.tsx
@@ -1,3 +1,10 @@
+import { Case } from "@/db/Case";
+import { Cpu } from "@/db/Cpu";
+import { Gpu } from "@/db/Gpu";
+import { Memory } from "@/db/Memory";
+import { Motherboard } from "@/db/Motherboard";
+import { Psu } from "@/db/Psu";
+import { Ssd } from "@/db/Ssd";
 import { selectedProductsAtom } from "@/jotai/atom";
 import { productType } from "@/types";
 import pages from "@/utils/pages.json";
@@ -5,6 +12,8 @@ import { ActionIcon, Container, Group, Title } from "@mantine/core";
 import { useClipboard } from "@mantine/hooks";
 import { useAtom } from "jotai";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import {
   BsCheckLg,
   BsLine,
@@ -16,13 +25,116 @@ import {
 import { SiMisskey } from "react-icons/si";
 import { TbBrandBluesky } from "react-icons/tb";
 import "reflect-metadata";
+import initSqlJs from "sql.js";
+import { Fetcher } from "swr";
+import useSWRImmutable from "swr/immutable";
+import { DataSource } from "typeorm";
 import { ProductCard } from "./ProductCard";
 import TotalPrice from "./TotalPrice";
 
+const fetcher: Fetcher<ArrayBuffer, string> = (url) =>
+  fetch(url).then((res) => res.arrayBuffer());
+
+function useBuf() {
+  const { data } = useSWRImmutable(
+    "https://bucket.nunawa.net/parts_latest.db",
+    fetcher,
+  );
+
+  return {
+    buf: data,
+  };
+}
+
 export default function BuildMain() {
   const buildList = [];
-  const [selectedProducts] = useAtom(selectedProductsAtom);
+  const [selectedProducts, setSelectedProducts] = useAtom(selectedProductsAtom);
   const clipboard = useClipboard();
+  const [sql, setSql] = useState<initSqlJs.SqlJsStatic>();
+  const { buf } = useBuf();
+  const [dataSource, setDatasource] = useState<DataSource>();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    initSqlJs({
+      locateFile: () =>
+        new URL("sql.js/dist/sql-wasm.wasm", import.meta.url).toString(),
+    }).then((sql) => {
+      setSql(sql);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!buf || !sql) {
+      return;
+    }
+
+    globalThis.SQL = sql;
+
+    (async () => {
+      const dataSource = new DataSource({
+        type: "sqljs",
+        entities: [Cpu, Memory, Motherboard, Gpu, Ssd, Psu, Case],
+        database: new Uint8Array(buf),
+      });
+
+      await dataSource.initialize();
+      setDatasource(dataSource);
+    })();
+  }, [buf, sql]);
+
+  useEffect(() => {
+    if (!dataSource || searchParams.size === 0) {
+      return;
+    }
+
+    const repositories = {
+      cpu: Cpu,
+      memory: Memory,
+      motherboard: Motherboard,
+      gpu: Gpu,
+      ssd: Ssd,
+      psu: Psu,
+      case: Case,
+    };
+
+    (async () => {
+      let newSelectedProducts = {
+        cpu: null,
+        memory: null,
+        motherboard: null,
+        gpu: null,
+        ssd: null,
+        psu: null,
+        case: null,
+      };
+
+      for (const key of Object.keys(newSelectedProducts)) {
+        const id = searchParams.get(key);
+        if (!id) {
+          continue;
+        }
+
+        const product = await dataSource.manager.findOneBy(
+          repositories[key as productType],
+          {
+            id: id,
+          },
+        );
+
+        if (!product) {
+          continue;
+        }
+
+        newSelectedProducts = {
+          ...newSelectedProducts,
+          [key]: product,
+        };
+      }
+
+      setSelectedProducts(newSelectedProducts);
+    })();
+  }, [dataSource, searchParams, setSelectedProducts]);
 
   const generateShareUrl = () => {
     const params = new URLSearchParams();
